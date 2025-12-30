f211272b2b5e516c3de1b3d40560e9fd6c036918
Selvanous <87690359+Selvanous@users.noreply.github.com>
2025-12-30 08:57:44 +0300

    Update README.md

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ app.use(bodyParser.json());
 
 app.get("/",function(req, res){
     res.send("hi ,i'm botchat")
+});
+    app.get("/webhook",function(req, res){
+        const verify_token = "sa.leh23"
+
+        let token = req.query['hub.verify_token'];
+        let challenge = req.query['hub.challenge'];
+
+        if(token === verify_token){
+            res.status(200).send(challenge);
+        }
+        else{
+            res.status(403);
+        }
 })
 
 app.listen(app.get("port"), function(){
 console.log("server is running and listening on port"+app.get("port"))
-});
+})
